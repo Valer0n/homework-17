2e1a5cb7bf9d2e2c19a3a061a3d15ecf5419c921
Valer0n <valera.zakrevskyi@gmail.com>
2022-02-01 15:46:07 +0200

    add homework 17

---
+++ min/js/script.js
@@ -0,0 +1,72 @@
+// 1. Завдання!
+// function userName() {
+// }
+// let userName = function () {
+// }
+// let userName = () => alert("Hello!");
+// let sum = (a, b) => a + b;
+
+
+// 2.Завдання
+// function calcSum() {
+//     console.log(arguments.length);
+// }
+// calcSum(3, 4, 5);
+
+// function calcSum() {
+//     let x = arguments[0];
+//     let y = arguments[1];
+//     let z = arguments[2];
+//     return x + y + z + n;
+// }
+// console.log(calcSum(11, 5, 12));
+
+// 3. Завдання!!
+// function checkNumb(a, b) {
+//     if (a < b) {
+//         console.log(-1);
+//     } else if (a > b) {
+//         console.log(1);
+//     } else if (a = b) {
+//         console.log(0);
+//     }
+// }
+// console.log(checkNumb(3, 2));
+
+
+// 4. Завдання!
+// let n = +prompt('Enter your number!')
+// function calcFactorial(n) {
+//     return (n != 1) ? n = n * calcFactorial(n - 1) : 1;
+// }
+// console.log(calcFactorial(n));
+// alert(calcFactorial(n));
+
+// 5.Завдання!
+// let a = +prompt('Enter first number!');
+// let b = +prompt('Enter second number!');
+// let c = +prompt('Enter third number!');
+
+// function createNumb(a, b, c) {
+//     a = String(a);
+//     b = String(b);
+//     c = String(c);
+//     return a + b + c;
+// }
+// console.log(createNumb(a, b, c));
+
+// 6. Завдання
+// let height = +prompt('Enter height of rectangle!');
+// let width = +prompt('Enter width of rectangle!');
+
+// function calcRectangle(height, width) {
+//     if (height && width) {
+//         return height * width;
+//     } else if (height) {
+//         return height * height;
+//     } else if (width) {
+//         return width * width;
+//     };
+// }
+// alert(calcRectangle(height, width));
+// console.log(calcRectangle(height, width));
---
+++ normal/js/script.js
@@ -0,0 +1,14 @@
+// let number = +prompt('Enter your number');
+
+// function perfectNumber(number) {
+//     let x = 0;
+//     for (i = 1; i <= number / 2; i++) {
+//         if (number % i === 0) {
+//             x += i;
+//         }
+//     }
+//     if (x === number && x !== 0) {
+//         console.log('It is perfect number');
+//     } else console.log('It is not perfect number');
+// }
+// perfectNumber(number);
